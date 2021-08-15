b56e94d429cada81cdefa954ad59dad5c5b6d07e
Ewelina Z <narcyza1989@o2.pl>
2021-08-15 18:32:45 +0200

    Add simple pagination for Comments

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,19 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class CommentController extends AbstractController
 {
+    private $twig;
+    private $entityManager;
+    private $comment;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+
     /**
      * @Route("/comment", name="comment")
      */
-    public function index(): Response
+    public function index(Request $request, CommentRepository $commentRepository): Response
     {
-        return $this->render('comment/index.html.twig', [
-            'controller_name' => 'CommentController',
-        ]);
+
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($offset);
+
+        return new Response($this->twig->render('comment/index.html.twig', [
+            //'comments' => $commentRepository->findAll(),
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+
+            ]));
     }
 }
--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -4,6 +4,8 @@ namespace App\Repository;
 
 use App\Entity\Comment;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -14,9 +16,24 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class CommentRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public const PAGINATOR_PER_PAGE = 2;
+
+    public function __construct(ManagerRegistry $registry )
     {
         parent::__construct($registry, Comment::class);
+    }
+
+    public function getCommentPaginator(int $offset): Paginator
+    {
+
+        $query = $this->createQueryBuilder('c')
+            ->orderBy('c.createdAt', 'ASC')
+            ->setMaxResults(self::PAGINATOR_PER_PAGE)
+            ->setFirstResult($offset)
+            ->getQuery()
+        ;
+
+        return new Paginator($query);
     }
 
     // /**
